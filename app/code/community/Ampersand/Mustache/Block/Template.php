a2e9a0dddee9b7ff3bc95ac9aababd5f548f6809
Yousef Cisco <yc@amp.co>
2014-09-16 09:50:26 +0000

    Updated mustache template rendering (b:23457 00:20)
    git-svn-id: https://ampersandit.svn.beanstalkapp.com/ampersand-enterprise/modules/Mustache/branches/development@4271 0a06cb53-a595-45d1-813f-372f60e41067

--- app/code/community/Ampersand/Mustache/Block/Template.php
+++ app/code/community/Ampersand/Mustache/Block/Template.php
@@ -1,6 +1,10 @@
 <?php
 class Ampersand_Mustache_Block_Template extends Mage_Core_Block_Template
 {
+
+    /** @var array */
+    protected $templateData = array();
+
     /**
      * @return string
      * @author Yousef Cisco <yc@amp.co>
@@ -11,20 +15,40 @@ class Ampersand_Mustache_Block_Template extends Mage_Core_Block_Template
 
         $engine = $this->helper('ampersand_mustache')->getEngine();
 
-        $html = $engine->render($html, $this->getStringData());
+        $html = $engine->render($html, $this->getTemplateData());
 
         return $html;
     }
 
-    public function getStringData()
+    /**
+     * @return Ampersand_Mustache_Block_Template
+     * @author Yousef Cisco <yc@amp.co>
+     */
+    public function setTemplateData($dataOrKey, $value = null)
     {
-        $stringData = array();
-        foreach ($this->getData() as $key => $value) {
-            if (is_string($value)) {
-                $stringData[$key] = $value;
-            }
+
+        if (!is_array($dataOrKey)) {
+            $data = array($dataOrKey => $value);
+        } else {
+            $data = $dataOrKey;
         }
 
-        return $stringData;
+        // Handle Varien Objects as we're likely to use them
+        if ($data instanceof Varien_Object) {
+            $data = $data->getData();
+        }
+
+        $this->templateData = array_merge($this->templateData, $data);
+
+        return $this;
     }
+
+    /**
+     * @author Yousef Cisco <yc@amp.co>
+     */
+    public function getTemplateData()
+    {
+        return $this->templateData;
+    }
+
 }
